0060758ee6d0dfb5d138eb3f5a615bd08b7339d3
Rempowa <rempowa@MBP-de-Rempowa.home>
2021-04-28 11:12:44 +0100

    Hover effect

--- src/Catogories/Category.js
+++ src/Catogories/Category.js
@@ -2,12 +2,27 @@ const Category = (props) => {
   if (!props.images || !Array.isArray(props.images)) {
     return;
   }
+
+  const getMovieName = (title) => {
+    title = title.split("/");
+    title = title[title.length - 1];
+
+    title = title.split("_");
+    title = title[0];
+    return title;
+  };
+
   return (
     <div className="category">
       <div className="name">{props.category}</div>
       <div className="movies">
         {props.images.map((img, i) => {
-          return <img key={i} src={img} alt="movi" />;
+          return (
+            <div key={i} className="photo">
+              <img src={img} alt={`movie ${i}`} />
+              <div className="moviename">{getMovieName(img)}</div>
+            </div>
+          );
         })}
       </div>
     </div>
